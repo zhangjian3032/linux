a54504de74f39461bc271c83e497e31407edebef
Ryan Chen <csf0711@gmail.com>
2017-09-21 15:10:17 +0800

    update

--- package.sh
+++ package.sh
@@ -20,7 +20,6 @@ if [ "$2" == "bmc" ]; then
 	find $1/. -name  '*_cam_*.h' | while read filename; do rm -f "${filename}"; done
 	find $1/. -name  '*_cam_*.c' | while read filename; do rm -f "${filename}"; done
 	rm -f $1/arch/arm/configs/ast2500_cam_defconfig
-	rm -f $1/drivers/crypto/ast-crypto.c
 	rm -f $1/drivers/usb/gadget/udc/ast_udc.c
 	rm -f $1/drivers/soc/aspeed/ast-jpeg.c
 	rm -f $1/arch/arm/configs/ast1220_defconfig
