4296c1dc494e480980333d1bb516eeb440de15ba
Ryan Chen <ryan_chen@aspeedtech.com>
2017-11-10 11:40:24 +0800

    add reset

--- drivers/irqchip/irq-aspeed-i2c.c
+++ drivers/irqchip/irq-aspeed-i2c.c
@@ -10,12 +10,17 @@
  * 2 of the License, or (at your option) any later version.
  *
  */
+#include <linux/platform_device.h>
+#include <linux/module.h>
+#include <linux/delay.h>
+#include <linux/reset.h>
 #include <linux/irq.h>
 #include <linux/irqchip.h>
 #include <linux/irqchip/chained_irq.h>
 #include <linux/irqdomain.h>
 #include <linux/of_address.h>
 #include <linux/of_irq.h>
+#include <linux/of.h>
 #include <linux/io.h>
 /*******************************************************************/
 #define AST_I2CG_ISR		0x00
@@ -36,14 +41,9 @@ struct ast_i2c_irq {
 	int			parent_irq;
 	int			bus_num;
 	struct irq_domain	*irq_domain;
+	struct reset_control *reset;	
 };
 /*******************************************************************/
-/* only support in ast-g5 platform */
-void ast_i2c_sram_buff_enable(struct ast_i2c_irq *i2c_irq) {
-	writel(I2C_SRAM_BUFF_EN, i2c_irq->regs + AST_I2CG_CTRL);
-};
-EXPORT_SYMBOL(ast_i2c_sram_buff_enable);
-
 static void ast_i2c_irq_handler(struct irq_desc *desc)
 {
 	struct ast_i2c_irq *i2c_irq = irq_desc_get_handler_data(desc);
@@ -68,15 +68,15 @@ static unsigned int noop_ret(struct irq_data *data)
 }
 
 struct irq_chip i2c_irq_chip = {
-	.name		= "i2c-irq",
+	.name			= "i2c-irq",
 	.irq_startup	= noop_ret,
 	.irq_shutdown	= noop,
-	.irq_enable	= noop,
+	.irq_enable		= noop,
 	.irq_disable	= noop,
-	.irq_ack	= noop,
-	.irq_mask	= noop,
-	.irq_unmask	= noop,
-	.flags		= IRQCHIP_SKIP_SET_WAKE,
+	.irq_ack		= noop,
+	.irq_mask		= noop,
+	.irq_unmask		= noop,
+	.flags			= IRQCHIP_SKIP_SET_WAKE,
 };
 
 static int ast_i2c_map_irq_domain(struct irq_domain *domain,
@@ -92,41 +92,10 @@ static const struct irq_domain_ops ast_i2c_irq_domain_ops = {
 	.map = ast_i2c_map_irq_domain,
 };
 
-#if 0
-static const struct ast_i2c_config ast_g3_i2c_config = { 
-	.dma_size = 0, 
-	.page_size = 256,
-	.page_no = 5,
-	.bus_num = 9,
-};
-
-static const struct ast_i2c_config ast_g4_i2c_config = { 
-	.dma_size = 0, 
-	.page_size = 256, 
-	.page_no = 8,
-	.bus_num = 14,
-};
-
-static const struct ast_i2c_config ast_g5_i2c_config = { 
-	.dma_size = 4095, 
-	.page_size = 16, 
-	.page_no = 14,
-	.bus_num = 14,
-};
-
-static const struct of_device_id ast_i2c_of_config[] = {
-	{ .compatible = "aspeed,ast-g3-i2c",	.data = &ast_g3_i2c_config, },
-	{ .compatible = "aspeed,ast-g4-i2c",	.data = &ast_g4_i2c_config, },
-	{ .compatible = "aspeed,ast-g5-i2c",	.data = &ast_g5_i2c_config, },
-	{}
-};
-
-#endif	
-static int __init ast_i2c_irq_of_init(struct device_node *node,
-					struct device_node *parent)
+static int irq_aspeed_i2c_probe(struct platform_device *pdev)
 {
 	struct ast_i2c_irq *i2c_irq;
-	u8 *buf_pool;
+	struct device_node *node = pdev->dev.of_node;
 
 	i2c_irq = kzalloc(sizeof(*i2c_irq), GFP_KERNEL);
 	if (!i2c_irq)
@@ -139,6 +108,22 @@ static int __init ast_i2c_irq_of_init(struct device_node *node,
 	node->data = i2c_irq;
 	if (of_property_read_u32(node, "bus_num", &i2c_irq->bus_num) == 0) {
 		I2C_IRQ_DBUG("i2c_irq->bus_num = %d \n", i2c_irq->bus_num);
+	}
+
+	i2c_irq->reset = devm_reset_control_get_exclusive(&pdev->dev, "i2c");
+	if (IS_ERR(i2c_irq->reset)) {
+		dev_err(&pdev->dev, "can't get i2c reset\n");
+		return PTR_ERR(i2c_irq->reset);
+	}
+
+	//SCU I2C Reset 
+	reset_control_assert(i2c_irq->reset);
+	udelay(3);
+	reset_control_deassert(i2c_irq->reset);
+	
+	if(of_machine_is_compatible("aspeed,ast2500")) {
+		/* only support in ast-g5 platform */
+		writel(I2C_SRAM_BUFF_EN, i2c_irq->regs + AST_I2CG_CTRL);
 	}
 
 	i2c_irq->parent_irq = irq_of_parse_and_map(node, 0);
@@ -156,16 +141,38 @@ static int __init ast_i2c_irq_of_init(struct device_node *node,
 	irq_set_chained_handler_and_data(i2c_irq->parent_irq,
 					 ast_i2c_irq_handler, i2c_irq);
 
-#if 0
-	buf_pool = of_iomap(node, 1);
-	if (!buf_pool)
-		buf_pool = 0;
-	
-	pool_buff_page_init((u32)buf_pool);
-#endif
 	pr_info("i2c-irq controller registered, irq %d\n", i2c_irq->parent_irq);
 
 	return 0;
 }
 
-IRQCHIP_DECLARE(ast_i2c_irq, "aspeed,ast-i2c-irq", ast_i2c_irq_of_init);
+static const struct of_device_id irq_aspeed_i2c_dt_ids[] = {
+	{ .compatible = "aspeed,ast-i2c-irq", },
+	{},
+};
+MODULE_DEVICE_TABLE(of, irq_aspeed_i2c_dt_ids);
+
+static struct platform_driver irq_aspeed_i2c_device_driver = {
+	.probe		= irq_aspeed_i2c_probe,
+//	.remove 	= irqc_remove,
+	.driver 	= {
+		.name	= "aspeed,ast-i2c-irq",
+		.of_match_table = irq_aspeed_i2c_dt_ids,
+	}
+};
+
+static int __init irq_aspeed_i2c_init(void)
+{
+	return platform_driver_register(&irq_aspeed_i2c_device_driver);
+}
+core_initcall(irq_aspeed_i2c_init);
+
+static void __exit irq_aspeed_i2c_exit(void)
+{
+	platform_driver_unregister(&irq_aspeed_i2c_device_driver);
+}
+module_exit(irq_aspeed_i2c_exit);
+
+MODULE_AUTHOR("Ryan Chen");
+MODULE_DESCRIPTION("AST SOC I2C IRQ Driver");
+MODULE_LICENSE("GPL v2");
