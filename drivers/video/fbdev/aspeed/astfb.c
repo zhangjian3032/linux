c478127684ef16daf30084ae198411b790567ce0
ryan_chen <csf0711@gmail.com>
2018-02-23 15:25:47 +0800

    [fix] astfb for boot crash

--- drivers/video/fbdev/aspeed/astfb.c
+++ drivers/video/fbdev/aspeed/astfb.c
@@ -1298,12 +1298,13 @@ static int astfb_probe(struct platform_device *pdev)
 			return -ENOMEM;
 	}
 
+	sfb = info->par;
+
 	if(of_device_is_compatible(pdev->dev.of_node, "aspeed,ast-g5-gfx"))
 		sfb->ast_g5 = 1;
 	else 
 		sfb->ast_g5 = 0;
 
-	sfb = info->par;
 	sfb->info = info;
 
 	switch(dev->id) {
