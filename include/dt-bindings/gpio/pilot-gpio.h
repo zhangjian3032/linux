e260ec43a784e6690ec31db7a6f6b6cabd164cdf
shivahs <shivahshankar.shankarnarayanrao@aspeedtech.com>
2019-04-17 15:25:18 +0800

    Added GPIO driver

---
+++ include/dt-bindings/gpio/pilot-gpio.h
@@ -0,0 +1,26 @@
+#ifndef _DT_BINDINGS_PINCTRL_ASPEED_PILOT_H
+#define _DT_BINDINGS_PINCTRL_ASPEED_PILOT_H
+
+//for padtype1
+#define CURR_DRV_4MA 0 
+#define CURR_DRV_6MA 1 
+#define CURR_DRV_8MA 2
+#define CURR_DRV_12MA 3
+
+// for Padtype2
+#define VG_DRV_1_8V_41_89_OHM  4
+#define VG_DRV_1_8V_36_77_OHM  5
+#define VG_DRV_1_8V_32_69_OHM  6
+#define VG_DRV_1_8V_29_62_OHM  7
+
+#define VG_DRV_2_5V_39_84_OHM  4
+#define VG_DRV_2_5V_34_73_OHM  5
+#define VG_DRV_2_5V_30_65_OHM  6
+#define VG_DRV_2_5V_28_59_OHM  7
+
+#define VG_DRV_3_3V_38_81_OHM  4
+#define VG_DRV_3_3V_33_70_OHM  5
+#define VG_DRV_3_3V_30_62_OHM  6
+#define VG_DRV_3_3V_27_56_OHM  7
+
+#endif
