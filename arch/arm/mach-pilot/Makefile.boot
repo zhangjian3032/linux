d1f9ad86e0097677087563e83e7d470c882e5aed
shivahs <shivahshankar.shankarnarayanrao@aspeedtech.com>
2019-06-25 19:08:43 +0800

    Added support for booting from 0x81008000 instead of 0x88008000 as it would unnecessary waste a lot of DDR memory

---
+++ arch/arm/mach-pilot/Makefile.boot
@@ -0,0 +1,4 @@
+   zreladdr-y	+= 0x81008000
+params_phys-y	:= 0x81000100
+initrd_phys-y	:= 0x89000000
+
---
+++ arch/arm/mach-pilot/include/mach/uncompress.h
@@ -0,0 +1,17 @@
+static inline void flush(void)
+{
+}
+
+/*
+ * This does not append a newline
+ */
+static inline void putc(int c)
+{
+}
+
+/*
+ * Setup for decompression
+ */
+static void arch_decomp_setup(void)
+{
+}
