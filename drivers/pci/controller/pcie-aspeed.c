da230bee7282a2c48f09465ba1fb72aad8561e66
ryan_chen <ryan_chen@aspeedtech.com>
2020-04-23 13:26:48 +0800

    update pcie rc driver

--- drivers/pci/controller/pcie-aspeed.c
+++ drivers/pci/controller/pcie-aspeed.c
@@ -284,7 +284,7 @@ static void aspeed_pcie_init_port(struct aspeed_pcie *pcie)
 	/* Don't register host if link is down */
 	if (readl(pcie->pciereg_base + ASPEED_PCIE_LINK) & PCIE_LINK_STS) {
 		aspeed_h2x_workaround(pcie);
-#if 0		
+#if 1		
 		if(readl(pcie->pciereg_base + ASPEED_PCIE_LINK_STS) & PCIE_LINK_5G)
 			printk("PCIE- Link up : 5G \n");
 		if(readl(pcie->pciereg_base + ASPEED_PCIE_LINK_STS) & PCIE_LINK_2_5G)
@@ -355,12 +355,12 @@ static int aspeed_pcie_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "can't get pcie reset\n");
 		return PTR_ERR(pcie->reset);
 	}
-
+#if 0
 	reset_control_assert(pcie->reset);
 	mdelay(50);
 	reset_control_deassert(pcie->reset);
 	mdelay(50);
-
+#endif
 	aspeed_pcie_init_port(pcie);
 
 	err = aspeed_pcie_init_irq_domain(pcie);
