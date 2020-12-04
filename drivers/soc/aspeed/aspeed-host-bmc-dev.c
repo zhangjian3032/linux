07e7d098795b77da9aca9690084f96496465e652
Chia-Wei, Wang <chiawei_wang@aspeedtech.com>
2020-12-04 10:50:46 +0800

    soc: aspeed: host-bmc-dev: Add LPC devices support
    1. Add IPMI-KCS over PCIe support
    2. Add virtual UART over PCIe support
    
    Signed-off-by: Chia-Wei, Wang <chiawei_wang@aspeedtech.com>

--- drivers/soc/aspeed/aspeed-host-bmc-dev.c
+++ drivers/soc/aspeed/aspeed-host-bmc-dev.c
@@ -22,7 +22,7 @@
 #include <linux/init.h>
 #include <linux/version.h>
 #include <linux/module.h>
-#include <linux/kernel.h>       /* printk()             */
+#include <linux/kernel.h>
 #include <linux/errno.h>
 
 #include <linux/pci.h>
@@ -32,6 +32,10 @@
 
 #include <linux/miscdevice.h>
 #include <linux/module.h>
+#include <linux/serial_core.h>
+#include <linux/serial_8250.h>
+
+#include "../../char/ipmi/ipmi_si.h"
 
 #define ASPEED_PCI_BMC_HOST2BMC_Q1		0x30000
 #define ASPEED_PCI_BMC_HOST2BMC_Q2		0x30010
@@ -63,15 +67,14 @@
 #define	 HOST2BMC_Q2_FULL_UNMASK		BIT(21)
 #define	 HOST2BMC_Q2_EMPTY_UNMASK		BIT(20)
 
-
 struct aspeed_pci_bmc_dev {
-	struct miscdevice miscdev;	
+	struct miscdevice miscdev;
 
-	unsigned long mem_bar_base; 
+	unsigned long mem_bar_base;
 	unsigned long mem_bar_size;
 	void __iomem *mem_bar_reg;
 
-	unsigned long message_bar_base; 
+	unsigned long message_bar_base;
 	unsigned long message_bar_size;
 	void __iomem *msg_bar_reg;
 
@@ -81,12 +84,25 @@ struct aspeed_pci_bmc_dev {
 	struct kernfs_node	*kn0;
 	struct kernfs_node	*kn1;
 
-	u8 IntLine;		
+	void *serial_priv;
+
+	u8 IntLine;
 };
 
-#define HOST_BMC_QUEUE_SIZE 			(16 * 4)
+#define HOST_BMC_QUEUE_SIZE			(16 * 4)
+
+#define BMC_MSI_INT 0
 
 #define DRIVER_NAME "ASPEED BMC DEVICE"
+
+static uint16_t ioport_kcs = 0x3a2;
+static uint16_t ioport_vuart = 0x3f8;
+
+module_param(ioport_kcs, ushort, 0644);
+MODULE_PARM_DESC(ioport_kcs, "The I/O port base address of IPMI-KCS over PCIe");
+
+module_param(ioport_vuart, ushort, 0644);
+MODULE_PARM_DESC(ioport_vuart, "The I/O port base address of VUART over PCIe");
 
 static struct aspeed_pci_bmc_dev *file_aspeed_bmc_device(struct file *file)
 {
@@ -147,7 +163,7 @@ static ssize_t aspeed_pci_bmc_dev_queue2_rx(struct file *filp, struct kobject *k
 		data[0] = readl(pci_bmc_device->msg_bar_reg + ASPEED_PCI_BMC_BMC2HOST_Q2);
 //		printk("Got BMC2HOST_Q2 [%x] \n", data[0]);
 		return 4;
-	} else 
+	} else
 		return 0;
 
 	return count;
@@ -157,11 +173,11 @@ static ssize_t aspeed_pci_bmc_dev_queue1_tx(struct file *filp, struct kobject *k
 		struct bin_attribute *attr, char *buf, loff_t off, size_t count)
 {
 	u32 tx_buff;
-	struct aspeed_pci_bmc_dev *pci_bmc_device = dev_get_drvdata(container_of(kobj, struct device, kobj));	
+	struct aspeed_pci_bmc_dev *pci_bmc_device = dev_get_drvdata(container_of(kobj, struct device, kobj));
 
 	if(count != 4)
 		return -1;
-	
+
 	if(readl(pci_bmc_device->msg_bar_reg + ASPEED_PCI_BMC_HOST2BMC_STS) & HOST2BMC_Q1_FULL)
 		return -1;
 	else {
@@ -179,11 +195,11 @@ static ssize_t aspeed_pci_bmc_dev_queue2_tx(struct file *filp, struct kobject *k
 		struct bin_attribute *attr, char *buf, loff_t off, size_t count)
 {
 	u32 tx_buff = 0;
-	struct aspeed_pci_bmc_dev *pci_bmc_device = dev_get_drvdata(container_of(kobj, struct device, kobj));	
+	struct aspeed_pci_bmc_dev *pci_bmc_device = dev_get_drvdata(container_of(kobj, struct device, kobj));
 
 	if(count != 4)
 		return -1;
-	
+
 	if(readl(pci_bmc_device->msg_bar_reg + ASPEED_PCI_BMC_HOST2BMC_STS) & HOST2BMC_Q2_FULL)
 		return -1;
 	else {
@@ -212,27 +228,26 @@ irqreturn_t aspeed_pci_host_bmc_device_interrupt(int irq, void* dev_id)
 		writel(BMC2HOST_ENABLE_INTB, pci_bmc_device->msg_bar_reg + ASPEED_PCI_BMC_BMC2HOST_STS);
 	}
 	if(bmc2host_q_sts & BMC2HOST_Q1_FULL) {
-	}	
+	}
 
 	if(bmc2host_q_sts & BMC2HOST_Q2_FULL) {
-	}	
+	}
 
 	return IRQ_HANDLED;
 
 }
 
-#define BMC_MSI_INT
-
-#define SCU_TRIGGER_MSI
 
 static int aspeed_pci_host_bmc_device_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 {
+	struct si_sm_io kcs_io;
+	struct uart_8250_port uart;
 	struct aspeed_pci_bmc_dev *pci_bmc_dev;
 	struct device *dev = &pdev->dev;
-	u16 config_cmd_val; 
+	u16 config_cmd_val;
 	int rc = 0;
 
-	dev_dbg(&pdev->dev, "ASPEED BMC PCI ID %04x:%04x\n", pdev->vendor, pdev->device);
+	dev_dbg(&pdev->dev, "ASPEED BMC PCI ID %04x:%04x, IRQ=%u\n", pdev->vendor, pdev->device, pdev->irq);
 
 	rc = pci_enable_device(pdev);
 	if (rc != 0) {
@@ -240,29 +255,31 @@ static int aspeed_pci_host_bmc_device_probe(struct pci_dev *pdev, const struct p
 		goto out_err;
 	}
 
-#ifdef BMC_MSI_INT
+#if BMC_MSI_INT
 	/* set PCI host mastering  */
 	pci_set_master(pdev);
 
-	rc = pci_enable_msi(pdev);
-	if(rc)
-		printk("MSI failed. et:%d\n", rc);
+	rc = pci_alloc_irq_vectors(pdev, 1, 1, PCI_IRQ_ALL_TYPES);
+	if (rc < 0)
+		printk("cannot allocate PCI MSI, rc=%d\n", rc);
 
-#ifdef SCU_TRIGGER_MSI
 	pci_read_config_word(pdev, PCI_COMMAND, &config_cmd_val);
 	printk("config_cmd_val %x \n", config_cmd_val);
 	config_cmd_val |= PCI_COMMAND_INTX_DISABLE;
 	printk("config_cmd_val %x \n", config_cmd_val);
 	pci_write_config_word((struct pci_dev *)pdev, PCI_COMMAND, config_cmd_val);
 #else
+	rc = pci_alloc_irq_vectors(pdev, 1, 1, PCI_IRQ_LEGACY);
+	if (rc < 0)
+		printk("cannot allocate PCI INTx, rc=%d\n", rc);
+
 	pci_read_config_word(pdev, PCI_COMMAND, &config_cmd_val);
 	printk("config_cmd_val %x \n", config_cmd_val);
 	config_cmd_val &= ~PCI_COMMAND_INTX_DISABLE;
 	printk("config_cmd_val %x \n", config_cmd_val);
 	pci_write_config_word((struct pci_dev *)pdev, PCI_COMMAND, config_cmd_val);
-#endif	
-
 #endif
+
 	pci_bmc_dev = kzalloc(sizeof(*pci_bmc_dev), GFP_KERNEL);
 	if (pci_bmc_dev == NULL) {
 			rc = -ENOMEM;
@@ -292,28 +309,31 @@ static int aspeed_pci_host_bmc_device_probe(struct pci_dev *pdev, const struct p
 	pci_bmc_dev->message_bar_size = pci_resource_len(pdev, 1);
 
     printk("MSG BAR1 Memory Mapped Base Address is: %08lx End %08lx\n", pci_bmc_dev->message_bar_base, pci_bmc_dev->message_bar_size);
-			
+
     pci_bmc_dev->msg_bar_reg = ioremap_nocache(pci_bmc_dev->message_bar_base, pci_bmc_dev->message_bar_size);
 	if (!pci_bmc_dev->msg_bar_reg) {
 		rc = -ENOMEM;
 		goto out_free1;
 	}
 
+	/* ERRTA40: dummy read */
+	rc = *((volatile uint32_t *)pci_bmc_dev->msg_bar_reg);
+
 	sysfs_bin_attr_init(&pci_bmc_dev->bin0);
 	sysfs_bin_attr_init(&pci_bmc_dev->bin1);
-	
+
 	pci_bmc_dev->bin0.attr.name = "pci-bmc-dev-queue1";
 	pci_bmc_dev->bin0.attr.mode = S_IRUSR | S_IWUSR;
 	pci_bmc_dev->bin0.read = aspeed_pci_bmc_dev_queue1_rx;
 	pci_bmc_dev->bin0.write = aspeed_pci_bmc_dev_queue1_tx;
 	pci_bmc_dev->bin0.size = 4;
-	
+
 	rc = sysfs_create_bin_file(&pdev->dev.kobj, &pci_bmc_dev->bin0);
 	if (rc) {
 		printk("error for bin file ");
 		goto out_free1;
 	}
-	
+
 	pci_bmc_dev->kn0 = kernfs_find_and_get(dev->kobj.sd, pci_bmc_dev->bin0.attr.name);
 	if (!pci_bmc_dev->kn0) {
 		sysfs_remove_bin_file(&dev->kobj, &pci_bmc_dev->bin0);
@@ -325,13 +345,13 @@ static int aspeed_pci_host_bmc_device_probe(struct pci_dev *pdev, const struct p
 	pci_bmc_dev->bin1.read = aspeed_pci_bmc_dev_queue2_rx;
 	pci_bmc_dev->bin1.write = aspeed_pci_bmc_dev_queue2_tx;
 	pci_bmc_dev->bin1.size = 4;
-	
+
 	rc = sysfs_create_bin_file(&pdev->dev.kobj, &pci_bmc_dev->bin1);
 	if (rc) {
 		sysfs_remove_bin_file(&dev->kobj, &pci_bmc_dev->bin1);
 		goto out_free1;
 	}
-	
+
 	pci_bmc_dev->kn1 = kernfs_find_and_get(dev->kobj.sd, pci_bmc_dev->bin1.attr.name);
 	if (!pci_bmc_dev->kn1) {
 		sysfs_remove_bin_file(&dev->kobj, &pci_bmc_dev->bin1);
@@ -348,14 +368,55 @@ static int aspeed_pci_host_bmc_device_probe(struct pci_dev *pdev, const struct p
 		pr_err("host bmc register fail %d\n",rc);
 		goto out_free;
 	}
-	
+
 	pci_set_drvdata(pdev, pci_bmc_dev);
 
-	rc = request_irq(pdev->irq, aspeed_pci_host_bmc_device_interrupt, 0, "ASPEED BMC DEVICE", pci_bmc_dev);
+	rc = request_irq(pdev->irq, aspeed_pci_host_bmc_device_interrupt, IRQF_SHARED, "ASPEED BMC DEVICE", pci_bmc_dev);
 	if (rc) {
 		pr_err("host bmc device Unable to get IRQ %d\n",rc);
 		goto out_unreg;
 	}
+
+	/* setup IPMI-KCS over PCIe */
+	memset(&kcs_io, 0, sizeof(kcs_io));
+	kcs_io.addr_source = SI_PCI;
+	kcs_io.addr_source_data = pdev;
+	kcs_io.si_type = SI_KCS;
+	kcs_io.addr_space = IPMI_MEM_ADDR_SPACE;
+	kcs_io.io_setup = ipmi_si_mem_setup;
+	kcs_io.addr_data = pci_bmc_dev->message_bar_base + (ioport_kcs << 2);
+	kcs_io.dev = &pdev->dev;
+	kcs_io.regspacing = 4;
+	kcs_io.regsize = 1;
+	kcs_io.regshift = 0;
+	kcs_io.irq = pci_irq_vector(pdev, 0);
+	if (kcs_io.irq)
+		kcs_io.irq_setup = ipmi_std_irq_setup;
+
+	rc = ipmi_si_add_smi(&kcs_io);
+	if (rc)
+		dev_err(dev, "cannot setup IPMI-KCS@%xh over PCIe, rc=%d\n", ioport_kcs, rc);
+
+	/* setup VUART */
+	memset(&uart, 0, sizeof(uart));
+
+	uart.port.flags = UPF_SKIP_TEST | UPF_BOOT_AUTOCONF | UPF_SHARE_IRQ;
+	uart.port.uartclk = 115200 * 16;
+
+	uart.port.irq = pci_irq_vector(pdev, 0);
+	uart.port.dev = &pdev->dev;
+
+	uart.port.iotype = UPIO_MEM32;
+	uart.port.iobase = 0;
+	uart.port.mapbase = pci_bmc_dev->message_bar_base + (ioport_vuart << 2);
+	uart.port.membase = 0;
+	uart.port.type = PORT_16550A;
+	uart.port.flags |= (UPF_IOREMAP | UPF_FIXED_PORT | UPF_FIXED_TYPE);
+	uart.port.regshift = 2;
+
+	rc = serial8250_register_8250_port(&uart);
+	if (rc < 0)
+		dev_err(dev, "cannot setup VUART@%xh over PCIe, rc=%d\n", ioport_vuart, rc);
 
 	return 0;
 
@@ -369,9 +430,9 @@ out_free:
 	kfree(pci_bmc_dev);
 out_err:
 	pci_disable_device(pdev);
-	
+
 	return rc;
-	
+
 }
 
 static void aspeed_pci_host_bmc_device_remove(struct pci_dev *pdev)
@@ -397,10 +458,10 @@ static struct pci_device_id aspeed_host_bmc_dev_pci_ids[] = {
 MODULE_DEVICE_TABLE(pci, aspeed_host_bmc_dev_pci_ids);
 
 static struct pci_driver aspeed_host_bmc_dev_driver = {
-	.name 		= DRIVER_NAME,
-	.id_table 	= aspeed_host_bmc_dev_pci_ids,
-	.probe 		= aspeed_pci_host_bmc_device_probe,
-	.remove 	= aspeed_pci_host_bmc_device_remove,
+	.name		= DRIVER_NAME,
+	.id_table	= aspeed_host_bmc_dev_pci_ids,
+	.probe		= aspeed_pci_host_bmc_device_probe,
+	.remove		= aspeed_pci_host_bmc_device_remove,
 };
 
 static int __init aspeed_host_bmc_device_init(void)
@@ -424,7 +485,7 @@ static void aspeed_host_bmc_device_exit(void)
 	pci_unregister_driver(&aspeed_host_bmc_dev_driver);
 }
 
-module_init(aspeed_host_bmc_device_init);
+late_initcall(aspeed_host_bmc_device_init);
 module_exit(aspeed_host_bmc_device_exit);
 
 MODULE_AUTHOR("Ryan Chen <ryan_chen@aspeedtech.com>");
