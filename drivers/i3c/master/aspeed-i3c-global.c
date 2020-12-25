5de94c2c56c3ef60cf6b992f590c18af02d9cd23
Dylan Hung <dylan_hung@aspeedtech.com>
2020-12-25 17:31:20 +0800

    migrate the changes of i3c driver from kernel v5.4

--- drivers/i3c/master/aspeed-i3c-global.c
+++ drivers/i3c/master/aspeed-i3c-global.c
@@ -23,8 +23,26 @@
 #include <linux/reset.h>
 #include <linux/delay.h>
 
-#define ASPEED_I3CG_CTRL(x)		(0x10 + (x*0x10))
-#define ASPEED_I3CG_SET(x)		(0x14 + (x*0x10))
+#define ASPEED_I3CG_CTRL(x)		(0x10 + (x * 0x10))
+#define ASPEED_I3CG_SET(x)		(0x14 + (x * 0x10))
+
+#define DEF_SLV_INST_ID			0x4
+#define DEF_SLV_STATIC_ADDR		0x74
+
+union i3c_set_reg {
+	uint32_t value;
+	struct {
+		unsigned int i2c_mode : 1;	/* bit[0] */
+		unsigned int test_mode : 1;	/* bit[1] */
+		unsigned int act_mode : 2;	/* bit[ 3: 2] */
+		unsigned int pending_int : 4;	/* bit[ 7: 4] */
+		unsigned int sa : 7;		/* bit[14: 8] */
+		unsigned int sa_en : 1;		/* bit[15] */
+		unsigned int inst_id : 4;	/* bit[19:16] */
+		unsigned int rsvd : 12;		/* bit[31:20] */
+	} fields;
+};
+
 
 struct aspeed_i3c_global {
 	void __iomem		*base;
@@ -40,19 +58,19 @@ static int aspeed_i3c_global_probe(struct platform_device *pdev)
 {
 	struct aspeed_i3c_global *i3c_global;
 	struct device_node *node = pdev->dev.of_node;
-	int i = 0;
+	union i3c_set_reg reg;
+	int i, ret;
+	u32 num_of_i3cs;
 
 	i3c_global = kzalloc(sizeof(*i3c_global), GFP_KERNEL);
 	if (!i3c_global)
 		return -ENOMEM;
 
 	i3c_global->base = of_iomap(node, 0);
-	if (!i3c_global->base) {
+	if (!i3c_global->base)
 		return -ENOMEM;
-	}
 
 	i3c_global->rst = devm_reset_control_get_exclusive(&pdev->dev, NULL);
-
 	if (IS_ERR(i3c_global->rst)) {
 		dev_err(&pdev->dev,
 			"missing or invalid reset controller device tree entry");
@@ -63,10 +81,20 @@ static int aspeed_i3c_global_probe(struct platform_device *pdev)
 	udelay(3);
 	reset_control_deassert(i3c_global->rst);
 
-	//init  
-	for(i = 0; i < 5; i++)
-		writel(0x000474c4, i3c_global->base + ASPEED_I3CG_SET(i));
-		
+	ret = of_property_read_u32(pdev->dev.of_node, "ni3cs", &num_of_i3cs);
+	if (ret < 0) {
+		dev_err(&pdev->dev, "unable to get number of i3c devices");
+		return -ENOMEM;
+	}
+
+	reg.value = 0;
+	reg.fields.inst_id = DEF_SLV_INST_ID;
+	reg.fields.sa = DEF_SLV_STATIC_ADDR;
+	reg.fields.pending_int = 0xc;
+	reg.fields.act_mode = 0x1;
+	for (i = 0; i < num_of_i3cs; i++)
+		writel(reg.value, i3c_global->base + ASPEED_I3CG_SET(i));
+
 	return 0;
 }
 
