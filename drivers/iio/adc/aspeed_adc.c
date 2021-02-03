3bf5eb4f6cf9119436f73aee144d2c2609945735
Billy Tsai <billy_tsai@aspeedtech.com>
2021-02-03 18:09:53 +0800

    iio: adc: aspeed: Fix the divide flag
    The adc divide value is (register value + 1), so we needs to use default
    flag 0 for clk_hw_register_divider api.
    
    Signed-off-by: Billy Tsai <billy_tsai@aspeedtech.com>

--- drivers/iio/adc/aspeed_adc.c
+++ drivers/iio/adc/aspeed_adc.c
@@ -505,7 +505,7 @@ static int aspeed_adc_probe(struct platform_device *pdev)
 			&pdev->dev, scaler_clk_name, clk_parent_name,
 			CLK_SET_RATE_UNGATE,
 			data->base + ASPEED_REG_CLOCK_CONTROL, 0, 16,
-			CLK_DIVIDER_ONE_BASED, &data->clk_lock);
+			0, &data->clk_lock);
 		if (IS_ERR(data->clk_scaler))
 			return PTR_ERR(data->clk_scaler);
 		/* ref_voltage config */
