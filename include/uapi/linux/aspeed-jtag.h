5d6b838d6894a08e5220ea39c4a989eaacb2101f
Billy Tsai <billy_tsai@aspeedtech.com>
2021-04-29 14:22:02 +0800

    drivers: jtag: Remove unused sir/sdr xfer api
    Signed-off-by: Billy Tsai <billy_tsai@aspeedtech.com>
    Change-Id: I05dbbd3cf913eefa634b5ffd9eeb084d35c40493

--- include/uapi/linux/aspeed-jtag.h
+++ include/uapi/linux/aspeed-jtag.h
@@ -43,23 +43,6 @@ struct jtag_xfer {
 	enum jtag_endstate end_sts;
 };
 
-struct sir_xfer {
-	enum jtag_xfer_mode mode;
-	unsigned short length;
-	unsigned int *tdi;
-	unsigned int *tdo;
-	unsigned char endir;
-};
-
-struct sdr_xfer {
-	enum jtag_xfer_mode mode;
-	unsigned char direct;
-	unsigned short length;
-	unsigned int *tdio;
-	unsigned char enddr;
-};
-
-
 struct io_xfer {
 	enum jtag_xfer_mode mode;
 	unsigned long Address;
@@ -74,15 +57,13 @@ struct trst_reset {
 #define JTAGIOC_BASE 'T'
 
 #define ASPEED_JTAG_IOCRUNTEST _IOW(JTAGIOC_BASE, 0, struct jtag_runtest_idle)
-#define ASPEED_JTAG_IOCSIR _IOWR(JTAGIOC_BASE, 1, struct sir_xfer)
-#define ASPEED_JTAG_IOCSDR _IOWR(JTAGIOC_BASE, 2, struct sdr_xfer)
-#define ASPEED_JTAG_SIOCFREQ _IOW(JTAGIOC_BASE, 3, unsigned int)
-#define ASPEED_JTAG_GIOCFREQ _IOR(JTAGIOC_BASE, 4, unsigned int)
-#define ASPEED_JTAG_IOWRITE _IOW(JTAGIOC_BASE, 5, struct io_xfer)
-#define ASPEED_JTAG_IOREAD _IOR(JTAGIOC_BASE, 6, struct io_xfer)
-#define ASPEED_JTAG_RESET _IOW(JTAGIOC_BASE, 7, struct io_xfer)
-#define ASPEED_JTAG_TRST_RESET _IOW(JTAGIOC_BASE, 8, struct trst_reset)
-#define ASPEED_JTAG_IOCXFER _IOWR(JTAGIOC_BASE, 9, struct jtag_xfer)
-#define ASPEED_JTAG_RUNTCK _IOW(JTAGIOC_BASE, 12, struct io_xfer)
+#define ASPEED_JTAG_IOCXFER _IOWR(JTAGIOC_BASE, 1, struct jtag_xfer)
+#define ASPEED_JTAG_SIOCFREQ _IOW(JTAGIOC_BASE, 2, unsigned int)
+#define ASPEED_JTAG_GIOCFREQ _IOR(JTAGIOC_BASE, 3, unsigned int)
+#define ASPEED_JTAG_IOWRITE _IOW(JTAGIOC_BASE, 4, struct io_xfer)
+#define ASPEED_JTAG_IOREAD _IOR(JTAGIOC_BASE, 5, struct io_xfer)
+#define ASPEED_JTAG_RESET _IOW(JTAGIOC_BASE, 6, struct io_xfer)
+#define ASPEED_JTAG_TRST_RESET _IOW(JTAGIOC_BASE, 7, struct trst_reset)
+#define ASPEED_JTAG_RUNTCK _IOW(JTAGIOC_BASE, 8, struct io_xfer)
 
 #endif /* _UAPI_LINUX_ASPEED_JTAG_H */
