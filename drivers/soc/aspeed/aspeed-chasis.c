76bb936ab54afa007a3d043a4c6d1a6c7a68dbd7
Billy Tsai <billy_tsai@aspeedtech.com>
2021-05-04 11:02:43 +0800

    soc: aspeed: chasis: Format the coding style
    Signed-off-by: Billy Tsai <billy_tsai@aspeedtech.com>
    Change-Id: Ia18e0c6ab754979831f86b5aa01f5af982b4486b

--- drivers/soc/aspeed/aspeed-chasis.c
+++ drivers/soc/aspeed/aspeed-chasis.c
@@ -38,40 +38,37 @@
 #include <asm/uaccess.h>
 /******************************************************************************/
 /* ASPEED_INTRUSION_CTRL */
-#define INTRUSION_STATUS_CLEAR  BIT(0)
-#define INTRUSION_INT_ENABLE    BIT(1)
-#define INTRUSION_STATUS        BIT(2)
-#define BATTERY_POWER_GOOD      BIT(3)
-#define CHASIS_RAW_STATUS       BIT(4)
+#define INTRUSION_STATUS_CLEAR BIT(0)
+#define INTRUSION_INT_ENABLE BIT(1)
+#define INTRUSION_STATUS BIT(2)
+#define BATTERY_POWER_GOOD BIT(3)
+#define CHASIS_RAW_STATUS BIT(4)
 #define CORE_POWER_STATUS_CLEAR BIT(8)
-#define CORE_POWER_INT_ENABLE   BIT(9)
-#define CORE_POWER_STATUS       BIT(10)
-#define IO_POWER_STATUS_CLEAR   BIT(16)
-#define IO_POWER_INT_ENABLE     BIT(17)
-#define IO_POWER_STATUS         BIT(18)
+#define CORE_POWER_INT_ENABLE BIT(9)
+#define CORE_POWER_STATUS BIT(10)
+#define IO_POWER_STATUS_CLEAR BIT(16)
+#define IO_POWER_INT_ENABLE BIT(17)
+#define IO_POWER_STATUS BIT(18)
 
-typedef union
-{
-	volatile uint32_t value;
-	struct
-	{
-		volatile uint32_t intrusion_status_clear:1; /*[0]*/
-        volatile uint32_t intrusion_int_enable:1; /*[1]*/ 
-        volatile uint32_t intrusion_status:1; /*[2]*/ 
-        volatile uint32_t battery_power_good:1; /*[3]*/ 
-        volatile uint32_t chasis_raw_status:1; /*[4]*/
-	    volatile uint32_t : 3; /*[5-7]*/
-	    volatile uint32_t io_power_status_clear:1; /*[8]*/ 
-        volatile uint32_t io_power_int_enable:1; /*[9]*/ 
-        volatile uint32_t core_power_status:1; /*[10]*/
-	    volatile uint32_t : 5; /*[11-15]*/
-	    volatile uint32_t core_power_status_clear:1; /*[16]*/  
-        volatile uint32_t core_power_int_enable:1; /*[17]*/ 
-        volatile uint32_t io_power_status:1; /*[18]*/
-	    volatile uint32_t : 13; /*[19-31]*/
+union chasis_ctrl_register {
+	uint32_t value;
+	struct {
+		uint32_t intrusion_status_clear : 1; /*[0]*/
+		uint32_t intrusion_int_enable : 1; /*[1]*/
+		uint32_t intrusion_status : 1; /*[2]*/
+		uint32_t battery_power_good : 1; /*[3]*/
+		uint32_t chasis_raw_status : 1; /*[4]*/
+		uint32_t reserved0 : 3; /*[5-7]*/
+		uint32_t io_power_status_clear : 1; /*[8]*/
+		uint32_t io_power_int_enable : 1; /*[9]*/
+		uint32_t core_power_status : 1; /*[10]*/
+		uint32_t reserved1 : 5; /*[11-15]*/
+		uint32_t core_power_status_clear : 1; /*[16]*/
+		uint32_t core_power_int_enable : 1; /*[17]*/
+		uint32_t io_power_status : 1; /*[18]*/
+		uint32_t reserved2 : 13; /*[19-31]*/
 	} fields;
-} chasis_ctrl_register_t;
-
+};
 
 struct aspeed_chasis {
 	struct device *dev;
@@ -81,37 +78,39 @@ struct aspeed_chasis {
 
 static void aspeed_chasis_status_check(struct aspeed_chasis *chasis)
 {
-    chasis_ctrl_register_t chasis_ctrl;
-    chasis_ctrl.value = readl(chasis->base);
-    if (chasis_ctrl.fields.intrusion_status) {
-	    dev_info(chasis->dev, "CHASI# pin has been pulled low");
-	    chasis_ctrl.fields.intrusion_status_clear = 1;
-	    writel(chasis_ctrl.value, chasis->base);
-        chasis_ctrl.fields.intrusion_status_clear = 0;
-        writel(chasis_ctrl.value, chasis->base);
-    }
-    chasis_ctrl.value = readl(chasis->base);
-    if (chasis_ctrl.fields.core_power_status) {
-	    dev_info(chasis->dev, "Core power has been pulled low");
-	    chasis_ctrl.fields.core_power_status_clear = 1;
-	    writel(chasis_ctrl.value, chasis->base);
-        chasis_ctrl.fields.core_power_status_clear = 0;
-        writel(chasis_ctrl.value, chasis->base);
-    }
+	union chasis_ctrl_register chasis_ctrl;
 
-    chasis_ctrl.value = readl(chasis->base);
-    if (chasis_ctrl.fields.io_power_status) {
-	    dev_info(chasis->dev, "IO power has been pulled low");
-	    chasis_ctrl.fields.io_power_status_clear = 1;
-	    writel(chasis_ctrl.value, chasis->base);
-        chasis_ctrl.fields.io_power_status_clear = 0;
-        writel(chasis_ctrl.value, chasis->base);
-    }
+	chasis_ctrl.value = readl(chasis->base);
+	if (chasis_ctrl.fields.intrusion_status) {
+		dev_info(chasis->dev, "CHASI# pin has been pulled low");
+		chasis_ctrl.fields.intrusion_status_clear = 1;
+		writel(chasis_ctrl.value, chasis->base);
+		chasis_ctrl.fields.intrusion_status_clear = 0;
+		writel(chasis_ctrl.value, chasis->base);
+	}
+	chasis_ctrl.value = readl(chasis->base);
+	if (chasis_ctrl.fields.core_power_status) {
+		dev_info(chasis->dev, "Core power has been pulled low");
+		chasis_ctrl.fields.core_power_status_clear = 1;
+		writel(chasis_ctrl.value, chasis->base);
+		chasis_ctrl.fields.core_power_status_clear = 0;
+		writel(chasis_ctrl.value, chasis->base);
+	}
+
+	chasis_ctrl.value = readl(chasis->base);
+	if (chasis_ctrl.fields.io_power_status) {
+		dev_info(chasis->dev, "IO power has been pulled low");
+		chasis_ctrl.fields.io_power_status_clear = 1;
+		writel(chasis_ctrl.value, chasis->base);
+		chasis_ctrl.fields.io_power_status_clear = 0;
+		writel(chasis_ctrl.value, chasis->base);
+	}
 }
 
 static irqreturn_t aspeed_chasis_isr(int this_irq, void *dev_id)
 {
 	struct aspeed_chasis *chasis = dev_id;
+
 	aspeed_chasis_status_check(chasis);
 	return IRQ_HANDLED;
 }
@@ -125,20 +124,20 @@ MODULE_DEVICE_TABLE(of, aspeed_chasis_of_table);
 static int aspeed_chasis_probe(struct platform_device *pdev)
 {
 	struct aspeed_chasis *chasis;
-	chasis_ctrl_register_t chasis_ctrl;
-    int ret;
+	union chasis_ctrl_register chasis_ctrl;
+	int ret;
 
 	dev_info(&pdev->dev, "aspeed_chasis_probe\n");
 
-    chasis = devm_kzalloc(&pdev->dev, sizeof(*chasis), GFP_KERNEL);
+	chasis = devm_kzalloc(&pdev->dev, sizeof(*chasis), GFP_KERNEL);
 	if (!chasis)
 		return -ENOMEM;
 
-    chasis->dev = &pdev->dev;
-    chasis->base = devm_platform_ioremap_resource(pdev, 0);
+	chasis->dev = &pdev->dev;
+	chasis->base = devm_platform_ioremap_resource(pdev, 0);
 	if (IS_ERR(chasis->base))
 		return PTR_ERR(chasis->base);
-	
+
 	chasis->irq = platform_get_irq(pdev, 0);
 	if (chasis->irq < 0) {
 		dev_err(&pdev->dev, "no irq specified\n");
@@ -146,28 +145,27 @@ static int aspeed_chasis_probe(struct platform_device *pdev)
 		return ret;
 	}
 
-	ret = devm_request_irq(&pdev->dev, chasis->irq, aspeed_chasis_isr,
-			       0, dev_name(&pdev->dev), chasis);
+	ret = devm_request_irq(&pdev->dev, chasis->irq, aspeed_chasis_isr, 0,
+			       dev_name(&pdev->dev), chasis);
 	if (ret) {
 		dev_err(&pdev->dev, "Chasis Unable to get IRQ");
 		return ret;
 	}
-    
-    aspeed_chasis_status_check(chasis);
+
+	aspeed_chasis_status_check(chasis);
 
 	// enable interrupt
-    chasis_ctrl.value = readl(chasis->base);
-    chasis_ctrl.fields.io_power_int_enable = 1;
-    chasis_ctrl.fields.intrusion_int_enable = 1;
-    chasis_ctrl.fields.core_power_int_enable = 1;
-    writel(chasis_ctrl.value, chasis->base);
-    dev_info(&pdev->dev, "Chasis ctrl value 0x%08x", chasis_ctrl.value);
+	chasis_ctrl.value = readl(chasis->base);
+	chasis_ctrl.fields.io_power_int_enable = 1;
+	chasis_ctrl.fields.intrusion_int_enable = 1;
+	chasis_ctrl.fields.core_power_int_enable = 1;
+	writel(chasis_ctrl.value, chasis->base);
+	dev_info(&pdev->dev, "Chasis ctrl value 0x%08x", chasis_ctrl.value);
 
-    dev_info(&pdev->dev, "chasis driver successfully loaded.\n");
+	dev_info(&pdev->dev, "chasis driver successfully loaded.\n");
 
-    return 0;
+	return 0;
 }
-
 
 static struct platform_driver aspeed_chasis_driver = {
 	.probe		= aspeed_chasis_probe,
