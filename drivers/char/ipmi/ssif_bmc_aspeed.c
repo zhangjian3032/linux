07175d6d15a0bb1e70762e74d3fb18382531b7e0
Ryan Chen <ryan_chen@aspeedtech.com>
2021-06-01 13:43:17 +0800

    enable auto nack
    Signed-off-by: Ryan Chen <ryan_chen@aspeedtech.com>
    Change-Id: I9fcfea2fd7a6f7c247900489a418c20ae11498c6

--- drivers/char/ipmi/ssif_bmc_aspeed.c
+++ drivers/char/ipmi/ssif_bmc_aspeed.c
@@ -70,7 +70,7 @@ static void aspeed_set_ssif_bmc_status(struct ssif_bmc_ctx *ssif_bmc, unsigned i
 		current_config |= ASPEED_I2CD_SLAVE_EN;
 		writel(current_config, bus->base + ASPEED_I2C_FUN_CTRL_REG);
 #ifdef CONFIG_I2C_NEW_ASPEED
-		writel(AST_I2CS_ACTIVE_ALL | AST_I2CS_PKT_MODE_EN | AST_I2CS_RX_DMA_EN, bus->base + AST_I2CS_CMD_STS);
+		writel(AST_I2CS_ACTIVE_ALL | AST_I2CS_PKT_MODE_EN | AST_I2CS_RX_DMA_EN | AST_I2CS_AUTO_NAK_EN, bus->base + AST_I2CS_CMD_STS);
 #endif
 	}
 
