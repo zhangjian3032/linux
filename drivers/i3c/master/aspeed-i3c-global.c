c5df868492800655516cad3f080c26f4af32ac80
Dylan Hung <dylan_hung@aspeedtech.com>
2021-09-09 15:30:20 +0800

    drivers/i3c/master: configure SDA pull-up resistor according to DT.
    for each i3c controller, there is a SDK pull-up resistor setting in the
    global controller.  Set up the resistor value according to
    "pull-up-resistors" DT property.
    
    Signed-off-by: Dylan Hung <dylan_hung@aspeedtech.com>
    Change-Id: Iae6da3f34c1224e8eb09e564d0182278a8fe9e16

--- drivers/i3c/master/aspeed-i3c-global.c
+++ drivers/i3c/master/aspeed-i3c-global.c
@@ -22,22 +22,26 @@
 #include <linux/io.h>
 #include <linux/reset.h>
 #include <linux/delay.h>
+#include <linux/slab.h>
 
-#define ASPEED_I3CG_CTRL(x)		(0x10 + (x * 0x10))
-#define ASPEED_I3CG_SET(x)		(0x14 + (x * 0x10))
+#define I3C_GLOBAL_REG0(x)		(0x10 + (x * 0x10))
+#define  SDK_PULLUP_EN_MASK		GENMASK(29, 28)
+#define  SDA_PULLUP_EN_2K		(0x1 << 28)
+#define  SDA_PULLUP_EN_750		(0x2 << 28)
+#define  SDA_PULLUP_EN_545		(0x3 << 28)
 
-#define DEF_SLV_INST_ID			0x4
-#define DEF_SLV_STATIC_ADDR		0x74
-
-union i3c_set_reg {
+#define I3C_GLOBAL_REG1(x)		(0x14 + (x * 0x10))
+union i3c_global_reg1 {
 	uint32_t value;
 	struct {
 		unsigned int i2c_mode : 1;	/* bit[0] */
 		unsigned int test_mode : 1;	/* bit[1] */
 		unsigned int act_mode : 2;	/* bit[ 3: 2] */
 		unsigned int pending_int : 4;	/* bit[ 7: 4] */
+#define DEF_SLV_STATIC_ADDR		0x74
 		unsigned int sa : 7;		/* bit[14: 8] */
 		unsigned int sa_en : 1;		/* bit[15] */
+#define DEF_SLV_INST_ID			0x4
 		unsigned int inst_id : 4;	/* bit[19:16] */
 		unsigned int rsvd : 12;		/* bit[31:20] */
 	} fields;
@@ -50,7 +54,7 @@ struct aspeed_i3c_global {
 };
 
 static const struct of_device_id aspeed_i3c_of_match[] = {
-	{ .compatible = "aspeed,ast2600-i3c-global", },	
+	{ .compatible = "aspeed,ast2600-i3c-global", },
 	{},
 };
 
@@ -58,9 +62,10 @@ static int aspeed_i3c_global_probe(struct platform_device *pdev)
 {
 	struct aspeed_i3c_global *i3c_global;
 	struct device_node *node = pdev->dev.of_node;
-	union i3c_set_reg reg;
+	union i3c_global_reg1 reg1;
+	u32 reg0, num_of_i3cs;
+	u32 *pullup_r_conf;
 	int i, ret;
-	u32 num_of_i3cs;
 
 	i3c_global = kzalloc(sizeof(*i3c_global), GFP_KERNEL);
 	if (!i3c_global)
@@ -87,13 +92,45 @@ static int aspeed_i3c_global_probe(struct platform_device *pdev)
 		return -ENOMEM;
 	}
 
-	reg.value = 0;
-	reg.fields.inst_id = DEF_SLV_INST_ID;
-	reg.fields.sa = DEF_SLV_STATIC_ADDR;
-	reg.fields.pending_int = 0xc;
-	reg.fields.act_mode = 0x1;
-	for (i = 0; i < num_of_i3cs; i++)
-		writel(reg.value, i3c_global->base + ASPEED_I3CG_SET(i));
+	pullup_r_conf = kmalloc(sizeof(u32) * num_of_i3cs, GFP_KERNEL);
+	if (!pullup_r_conf)
+		return -ENOMEM;
+
+	ret = of_property_read_u32_array(pdev->dev.of_node, "pull-up-resistors",
+					 pullup_r_conf, num_of_i3cs);
+	if (ret < 0) {
+		dev_warn(&pdev->dev,
+			 "use 2K Ohm SDA pull up resistor by default");
+		for (i = 0; i < num_of_i3cs; i++)
+			pullup_r_conf[i] = 2000;
+	}
+
+	reg1.value = 0;
+	reg1.fields.inst_id = DEF_SLV_INST_ID;
+	reg1.fields.sa = DEF_SLV_STATIC_ADDR;
+	reg1.fields.pending_int = 0xc;
+	reg1.fields.act_mode = 0x1;
+
+	for (i = 0; i < num_of_i3cs; i++) {
+		reg0 = readl(i3c_global->base + I3C_GLOBAL_REG0(i));
+		reg0 &= SDK_PULLUP_EN_MASK;
+		switch (pullup_r_conf[i]) {
+		case 750:
+			reg0 |= SDA_PULLUP_EN_750;
+			break;
+		case 545:
+			reg0 |= SDA_PULLUP_EN_545;
+			break;
+		case 2000:
+		default:
+			reg0 |= SDA_PULLUP_EN_2K;
+			break;
+		}
+		writel(reg0, i3c_global->base + I3C_GLOBAL_REG0(i));
+		writel(reg1.value, i3c_global->base + I3C_GLOBAL_REG1(i));
+	}
+
+	kfree(pullup_r_conf);
 
 	return 0;
 }
