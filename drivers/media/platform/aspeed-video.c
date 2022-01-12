zhangjian3032/linux

commit b195e2ac0251bbd55536df8d09e45ec3fe088ecc

medai: aspeed: Fix using sync mode if refresh-rate changed only
If only refresh-rate is different in timings change, user could skip
set_dv_timings since the size of frame is the same. In this way,
capture mode will not be updated per frame-size and stay at default
, sync, mode.

To fix the problem, we just go aspeed_video_set_resolution() if
aspeed_video_resolution_work() finishes its job.

Signed-off-by: Jammy Huang <jammy_huang@aspeedtech.com>
Change-Id: Iaee4f42840011ffddba3b715510b97d29fdbaee6

drivers/media/platform/aspeed-video.c
@@ -1491,8 +1491,11 @@ static int aspeed_video_set_dv_timings(struct file *file, void *fh,
 	struct aspeed_video *video = video_drvdata(file);
 
 	if (timings->bt.width == video->active_timings.width &&
-	    timings->bt.height == video->active_timings.height)
+	    timings->bt.height == video->active_timings.height) {
+		v4l2_info(&video->v4l2_dev, "%s: skip update for identical timings(%dx%d)\n",
+			  __func__, timings->bt.width, timings->bt.height);
 		return 0;
+	}
 
 	if (vb2_is_busy(&video->queue))
 		return -EBUSY;
@@ -1699,6 +1702,7 @@ static void aspeed_video_resolution_work(struct work_struct *work)
 	struct delayed_work *dwork = to_delayed_work(work);
 	struct aspeed_video *video = container_of(dwork, struct aspeed_video,
 						  res_work);
+	bool is_res_chg = false;
 
 	aspeed_video_on(video);
 
@@ -1712,8 +1716,17 @@ static void aspeed_video_resolution_work(struct work_struct *work)
 
 	aspeed_video_get_resolution(video);
 
-	if (video->detected_timings.width != video->active_timings.width ||
-	    video->detected_timings.height != video->active_timings.height) {
+	if (video->v4l2_input_status) {
+		v4l2_info(&video->v4l2_dev, "%s: res-detection failed\n", __func__);
+		goto done;
+	}
+
+	is_res_chg = memcmp(&video->active_timings, &video->detected_timings,
+			    sizeof(video->active_timings));
+	video->active_timings = video->detected_timings;
+	aspeed_video_set_resolution(video);
+
+	if (is_res_chg) {
 		static const struct v4l2_event ev = {
 			.type = V4L2_EVENT_SOURCE_CHANGE,
 			.u.src_change.changes = V4L2_EVENT_SRC_CH_RESOLUTION,

commit 12d521a514b0eaa9317c1170fc1289e6d0cb299f

media: aspeed: Fix video stopped if changing refresh rate
If refresh rate is changed, obmc-ikvm will not restart since frame
size is the same. Video will stop.

To fix the problem, we compare only width/height in
aspeed_video_get_resolution(), since other timings information
doesn't affect frame-grabbing.

Fixes: b195e2ac0251b (medai: aspeed: Fix using sync mode if
refresh-rate changed only)
Change-Id: Ib63457e2ead398a9d893072583ac9276028e9a88
Signed-off-by: Jammy Huang <jammy_huang@aspeedtech.com>
(cherry picked from commit 404cf09ce7aa45e8cb6cec6edd4a3129dc2e5373)

drivers/media/platform/aspeed-video.c
@@ -1741,8 +1741,8 @@ static void aspeed_video_resolution_work(struct work_struct *work)
 	if (video->v4l2_input_status)
 		goto done;
 
-	is_res_chg = memcmp(&video->active_timings, &video->detected_timings,
-			    sizeof(video->active_timings));
+	is_res_chg = (video->detected_timings.width != video->active_timings.width ||
+		      video->detected_timings.height != video->active_timings.height);
 	video->active_timings = video->detected_timings;
 	aspeed_video_set_resolution(video);
 